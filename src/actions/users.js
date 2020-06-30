2857b8e7949b6b71a34d50d5625c9abd973be5b1
sahinaksoy <sahinaksoy@gmail.com>
2020-06-30 12:36:18 +0300

    feat: Save questions

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,3 +1,6 @@
+import { saveQuestionAnswer } from '../utils/api';
+import { addAnswerToQuestion } from '../actions/questions';
+
 export const RECEIVE_USERS = 'RECEIVE_USERS';
 export const ADD_ANSWER_TO_USER = 'ADD_ANSWER_TO_USER';
 export const ADD_QUESTION_TO_USER = 'ADD_QUESTION_TO_USER';
@@ -21,7 +24,11 @@ function addAnswerToUser(authUser, qid, answer) {
 export function handleSaveQuestionAnswer(authUser, qid, answer) {
   return dispatch => {
     dispatch(addAnswerToUser(authUser, qid, answer));
-    //TODO: save questions
+    dispatch(addAnswerToQuestion(authUser, qid, answer));
+
+    return saveQuestionAnswer(authUser, qid, answer).catch(e => {
+      console.warn('Error in handleSaveQuestionAnswer:', e);
+    });
   };
 }
 
