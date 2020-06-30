58f3ecca6c97c9f882f369af2be0260b04f94a04
sahinaksoy <sahinaksoy@gmail.com>
2020-06-30 13:53:01 +0300

    refactor: add app component to index file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./index.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/index";
+import middleware from "./middleware";
+
+const store = createStore(rootReducer, middleware);
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
-
